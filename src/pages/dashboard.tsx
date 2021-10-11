0e8fbb8af91c2dccf271c3a4c4e843cd81dcbf3f
Farell Faiz <farellfaiz@gmail.com>
2021-10-11 17:29:12 +0700

    update basic info

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,7 @@ import { MetaHead, NO_INDEXED } from 'components/MetaHead/MetaHead'
 function Dashboard() {
   return (
     <LayoutAuth>
-      <MetaHead title="Dashboard | Ksana.in" robots={NO_INDEXED} />
+      <MetaHead title="Dashboard | Massa Bumi 2021 Shortener" robots={NO_INDEXED} />
       <Stack
         spacing={8}
         mx={'auto'}
